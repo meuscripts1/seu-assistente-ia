c6b51700131354398c056a501ab34611b1f17936
Nobru <daniel349camilo@gmail.com>
2026-05-05 20:57:21 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import Index from "./pages/Index.tsx";
-import NotFound from "./pages/NotFound.tsx";
+import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
+import { AuthProvider } from "./hooks/useAuth";
+import { AuthButton } from "./components/AuthButton";
 
 const queryClient = new QueryClient();
 
@@ -13,13 +15,16 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <AuthButton />
+          <Routes>
+            <Route path="/" element={<Index />} />
+            {/* ADICIONE ROTAS CUSTOMIZADAS ACIMA DA ROTA "*" */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
     </TooltipProvider>
   </QueryClientProvider>
 );
