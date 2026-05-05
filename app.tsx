ad67c10d810c08383642eb7bddfcb6ba50c6361a
Nobru <daniel349camilo@gmail.com>
2026-05-05 20:11:44 -0300

    Initialize main application structure with routing

---
+++ app.tsx
@@ -0,0 +1,32 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
+import { AuthProvider } from "./hooks/useAuth";
+import { AuthButton } from "./components/AuthButton";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <AuthProvider>
+        <BrowserRouter>
+          <AuthButton />
+          <Routes>
+            <Route path="/" element={<Index />} />
+            {/* ADICIONE ROTAS CUSTOMIZADAS ACIMA DA ROTA "*" */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
